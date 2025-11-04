2e6a0b6f6025bbe520d880193255c18708685c40
unknown <zokirjonovmuhammadrizo6@gmail.com>
2025-11-04 14:11:37 +0500

    test

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     domains: ['supabase.co'],
   },
+  typescript: {
+    // Allow production builds to successfully complete even if there are type errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
